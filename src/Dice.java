f5e7877051e9ed9b8331db040dcf4581b6531416
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2019-12-15 19:47:45 -0800

    Added algorithmic complexity

--- src/Dice.java
+++ src/Dice.java
@@ -1,62 +1,70 @@
 import java.util.Random;
 public class Dice
 {
-        private int sides;
-        private Random generator = new Random();
-        private int roll = 0;
+    private int sides;
+    private Random generator = new Random();
+    private int roll = 0;
 
-        //constructors
-        public Dice()
+    //constructors
+    public Dice()
+    {
+        sides = 2;
+    }//end of 0-arg
+    public Dice(int newSides)
+    {
+        sides = checkSides(newSides);
+    }//end of single -arg
+    //rolls a random number within the max and min
+    public int roll(int num)
+    {
+        roll = 0;
+        int tempRoll;
+        for(int i = 0; i < num; i++)
         {
-            sides = 2;
-        }//end of 0-arg
-        public Dice(int newSides)
-        {
-            sides = checkSides(newSides);
-        }//end of single -arg
-        //rolls a random number within the max and min
-        public int roll(int num)
-        {
-            roll = 0;
-            int tempRoll;
-            for(int i = 0; i < num; i++)
+            tempRoll = generator.nextInt(sides) + 1;
+            if(sides == 20 && tempRoll == sides)
             {
-                tempRoll = generator.nextInt(sides) + 1;
-                roll += tempRoll;
+                tempRoll = tempRoll * 2;
             }
-            return roll;
-        }//end of roll
-        //getters
-        //does not reroll, simply returns the current value of the roll.
-        public int getRoll()
-        {
-            return roll;
-        }
-        public int getSides()
-        {
-            return sides;
-        }//end of get sides getter
-        //setters
-        public void setSides(int newSides)
-        {
-            sides = checkSides(newSides);
-        }//end of setSides setter
-        //brain methods
-        private int checkSides(int newSides)
-        {
-            int tempSides = newSides;
-            if(tempSides < 1)
+            else if(sides == 20 && tempRoll == 1)
             {
-                tempSides = 2;
-            }//end of if
-
-            return tempSides;
-        }//end of check sides
-        //to string
-        public String toString()
+                tempRoll = 0;
+            }
+            roll += tempRoll;
+        }
+        return roll;
+    }//end of roll
+    //getters
+    //does not reroll, simply returns the current value of the roll.
+    public int getRoll()
+    {
+        return roll;
+    }
+    public int getSides()
+    {
+        return sides;
+    }//end of get sides getter
+    //setters
+    public void setSides(int newSides)
+    {
+        sides = checkSides(newSides);
+    }//end of setSides setter
+    //brain methods
+    private int checkSides(int newSides)
+    {
+        int tempSides = newSides;
+        if(tempSides < 1)
         {
-            String output;
-            output = "Die with " + sides + " sides";
-            return output;
-        }//end of toString
+            tempSides = 2;
+        }//end of if
+
+        return tempSides;
+    }//end of check sides
+    //to string
+    public String toString()
+    {
+        String output;
+        output = "Die with " + sides + " sides";
+        return output;
+    }//end of toString
 }
