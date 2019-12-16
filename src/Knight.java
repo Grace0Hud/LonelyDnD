51e514b36779af37f9e183612160d3fcc46b755d
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2019-12-15 19:52:57 -0800

    algorithmic complexity

--- src/Knight.java
+++ src/Knight.java
@@ -1,8 +1,9 @@
 //a knight character.
 //Skill bonuses
-    //+3 str
-    //+2 char
-public class Knight implements userClass {
+//+3 str
+//+2 char
+public class Knight implements userClasses
+{
     //dice for character creation and attacks
     private Dice d20 = new Dice(20);
     private Dice d12 = new Dice(12);
@@ -25,7 +26,7 @@ public class Knight implements userClass {
     }//end of defalt constructor
 
     public Knight(String name, String race, int level) {
-        this.name = name;
+        this.name = checkName(name);
         this.race = race;
         this.level = level;
         setUp();
@@ -76,6 +77,15 @@ public class Knight implements userClass {
     {
         return level;
     }//end level getter
+    //setters
+    public void setName(String name)
+    {
+        this.name = checkName(name);
+    }
+    public void setRace(String race)
+    {
+        this.race = race;
+    }
     public void levelUp(int level)
     {
         this.level += level;
@@ -103,6 +113,20 @@ public class Knight implements userClass {
         hp = d12.roll(level) + (con / 10 + con % 10);
         temphp = hp;
     }
+    //checks for name length
+    private String checkName(String name)
+    {
+        String tempName = name;
+        if(tempName.length() > 30)
+        {
+            name = tempName.substring(0, 30);
+        }
+        else if(tempName.length() < 1)
+        {
+            name = "Sam Smorkle";
+        }
+        return name;
+    }
     // controls if the character will receive damage
     //toHit is rolled by the enemy using their attack stat
     //if it is less than the AC of the character, it does not hit.
@@ -127,8 +151,8 @@ public class Knight implements userClass {
     }
     public int rollDamage(String weapon)
     {
-       int damage = howMuchDamage(weapon);
-       return damage;
+        int damage = howMuchDamage(weapon);
+        return damage;
     }
     //decides how much extra help the user gets based on their weapon
     private int howMuchHit(String weapon)
@@ -151,14 +175,11 @@ public class Knight implements userClass {
     private int howMuchDamage(String weapon)
     {
         int damage = 0;
-        if(weapon.equals("stab"))
-        {
-            damage = d8.roll(3);
-        }
-        else if(weapon.equals("fist"))
-        {
-            damage = d8.roll(1);
-        }
+            if (weapon.equals("stab")) {
+                damage = d8.roll(3);
+            } else if (weapon.equals("fist")) {
+                damage = d8.roll(1);
+            }
         return damage;
     }
     //prints out character sheet
